f6807a24ca8403906ba8f20496a578cfef633d08
oaleoliveira <abasolucoes@gmail.com>
2026-07-24 18:00:52 -0300

    a

--- assets/js/novidades-data.js
+++ assets/js/novidades-data.js
@@ -1,32 +1,32 @@
 // Imagens exibidas no card "Novidades" da home. Editado via admin > Novidades.
 window.NOVIDADES = [
   {
-    "img": "/site-tia-maria/assets/images/novidades/doces-gourmet.jpg",
+    "img": "/assets/images/novidades/doces-gourmet.jpg",
     "alt": "Doces Gourmet",
     "legenda": "Doces Gourmet"
   },
   {
-    "img": "/site-tia-maria/assets/images/novidades/festa-individual.jpg",
+    "img": "/assets/images/novidades/festa-individual.jpg",
     "alt": "festa na caixa",
     "legenda": "festa na caixa"
   },
   {
-    "img": "/site-tia-maria/assets/images/novidades/bem-casado-01.jpg",
+    "img": "/assets/images/novidades/bem-casado-01.jpg",
     "alt": "Bem casado",
     "legenda": "Bem casado"
   },
   {
-    "img": "/site-tia-maria/assets/images/novidades/bolo-especial-04.jpg",
+    "img": "/assets/images/novidades/bolo-especial-04.jpg",
     "alt": "Bolo especial",
     "legenda": "Bolo especial"
   },
   {
-    "img": "/site-tia-maria/assets/images/novidades/bolo-personalizado-01.jpg",
+    "img": "/assets/images/novidades/bolo-personalizado-01.jpg",
     "alt": "Bolo Personalizado",
     "legenda": "Bolo Personalizado"
   },
   {
-    "img": "/site-tia-maria/assets/images/novidades/doces-gourmet-10.jpg",
+    "img": "/assets/images/novidades/doces-gourmet-10.jpg",
     "alt": "Doces gourmet",
     "legenda": "Doces gourmet"
   }
