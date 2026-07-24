2d0dcf053c7758adb81c3e86e4931e99fc2157e9
oaleoliveira <abasolucoes@gmail.com>
2026-07-24 17:45:32 -0300

    a

--- assets/js/novidades-data.js
+++ assets/js/novidades-data.js
@@ -1,18 +1,33 @@
 // Imagens exibidas no card "Novidades" da home. Editado via admin > Novidades.
 window.NOVIDADES = [
   {
-    img: "/site-tia-maria/assets/images/bolo-tradicional/05-2022/bolos-tradicionais1.jpg",
-    alt: "Bolo decorado Tia Maria",
-    legenda: "Bolo tradicional feito à mão"
+    "img": "/site-tia-maria/assets/images/novidades/doces-gourmet.jpg",
+    "alt": "Doces Gourmet",
+    "legenda": "Doces Gourmet"
   },
   {
-    img: "/site-tia-maria/assets/images/bolo-tradicional/05-2022/bolos-tradicionais3.jpg",
-    alt: "Bolo Tia Maria",
-    legenda: "Sempre fresquinho, sempre com carinho"
+    "img": "/site-tia-maria/assets/images/novidades/festa-individual.jpg",
+    "alt": "festa na caixa",
+    "legenda": "festa na caixa"
   },
   {
-    img: "/site-tia-maria/assets/images/festa-caixa.jpg",
-    alt: "Kit Festa Tia Maria",
-    legenda: "Kits prontos para sua festa"
+    "img": "/site-tia-maria/assets/images/novidades/bem-casado-01.jpg",
+    "alt": "Bem casado",
+    "legenda": "Bem casado"
+  },
+  {
+    "img": "/site-tia-maria/assets/images/novidades/bolo-especial-04.jpg",
+    "alt": "Bolo especial",
+    "legenda": "Bolo especial"
+  },
+  {
+    "img": "/site-tia-maria/assets/images/novidades/bolo-personalizado-01.jpg",
+    "alt": "Bolo Personalizado",
+    "legenda": "Bolo Personalizado"
+  },
+  {
+    "img": "/site-tia-maria/assets/images/novidades/doces-gourmet-10.jpg",
+    "alt": "Doces gourmet",
+    "legenda": "Doces gourmet"
   }
 ];
